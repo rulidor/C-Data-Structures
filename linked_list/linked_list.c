a38f9756a0bca6d7d202bb75d6f1dec9b72a162b
rulidor <rulidor90@gmail.com>
2022-10-01 15:52:17 +0300

    linked list: added key to Node struct

--- linked_list/linked_list.c
+++ linked_list/linked_list.c
@@ -5,17 +5,19 @@
 
 struct Node {
     void* data;
+    char key[20];
     struct Node* next;
 };
 
-void insert_at_beginning(struct Node **head, void* data, size_t data_size);
-void insert_at_end(struct Node **head, void* data, size_t data_size);
-void delete_by_index(struct Node *head, int index); //todo
+void insert_at_beginning(struct Node **head, void* data, size_t data_size, char* key);
+void insert_at_end(struct Node **head, void* data, size_t data_size, char* key);
+void delete_by_key(struct Node *head, int index); //todo
 void print_list(struct Node *head, char format);
 
 
-void insert_at_beginning(struct Node **head, void* data, size_t data_size){
+void insert_at_beginning(struct Node **head, void* data, size_t data_size, char* key){
     struct Node* new_node = malloc(sizeof(struct Node));
+    strcpy(new_node->key, key);
     new_node->data = malloc(data_size);
     for (int i = 0; i < data_size; i++){
         *((char *)(new_node->data) + i) = * ((char *)(data) + i);
@@ -25,9 +27,10 @@ void insert_at_beginning(struct Node **head, void* data, size_t data_size){
     *head = new_node;
 }
 
-void insert_at_end(struct Node **head, void* data, size_t data_size){
+void insert_at_end(struct Node **head, void* data, size_t data_size, char* key){
     struct Node* new_node = malloc(sizeof(struct Node));
     new_node->next = NULL; //new node is going to be the last node
+    strcpy(new_node->key, key);
     new_node->data = malloc(data_size);
     for (int i = 0; i < data_size; i++){
         *((char *)(new_node->data) + i) = * ((char *)(data) + i);
@@ -49,14 +52,15 @@ void insert_at_end(struct Node **head, void* data, size_t data_size){
 void print_list(struct Node *head, char format){
     struct Node* tmp = head;
     while (tmp != NULL){
+        printf("--> key: %s, value: ", tmp->key);
         switch (format)
         {
         case 's':
-            printf("--> %s", *(char*)(tmp->data)); //todo: check if casting to pointer to string needed: char**
+            printf("%s", *(char*)(tmp->data)); //todo: check if casting to pointer to string needed: char**
             break;
 
         case 'd':
-            printf("--> %d", *(int*)(tmp->data));
+            printf("%d", *(int*)(tmp->data));
             break;
         
         default:
@@ -74,16 +78,16 @@ int main(){
     struct Node* head = NULL;
     int data = 10;
     void* p_data = &data;
-    insert_at_beginning(&head, p_data, sizeof(int));
+    insert_at_beginning(&head, p_data, sizeof(int), "0");
     data = 20;
-    insert_at_beginning(&head, p_data, sizeof(int));
+    insert_at_beginning(&head, p_data, sizeof(int), "1");
     data = 30;
-    insert_at_beginning(&head, p_data, sizeof(int));
+    insert_at_beginning(&head, p_data, sizeof(int), "2");
 
     data = 7;
-    insert_at_end(&head, p_data, sizeof(int));
+    insert_at_end(&head, p_data, sizeof(int), "3");
     data = 3;
-    insert_at_end(&head, p_data, sizeof(int));
+    insert_at_end(&head, p_data, sizeof(int), "4");
 
     print_list(head, 'd');
 }
